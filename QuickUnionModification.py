52f8f2d6095c2c06c33d282641d6031bfe06eee2
Kutuzov Vlad <alphagrimsaw@gmail.com>
2020-02-14 00:41:02 +0200

    Designed one algorithm

--- QuickUnionModification.py
+++ QuickUnionModification.py
@@ -1,14 +1,8 @@
-class QuickUnionUF:
-    '''Python better implementation of union-find algortihm
-    '''
-
+class QuickUnionUFModification:
     def __init__(self, num=0):
         self.num = num
         self.id = [i for i in range(num)]
-
-    def setNum(self, num):
-        self.num = num
-        self.id = [i for i in range(num)]
+        self.sz = [0] * num
 
     def __root(self, i):
         while i != self.id[i]:
@@ -21,5 +15,11 @@ class QuickUnionUF:
     def union(self, p, q):
         i = self.__root(p)
         j = self.__root(q)
-        self.id[i] = j
-
+        if i == j:
+            return
+        if self.sz[i] < self.sz[j]:
+            self.id[i] = j
+            self.sz[j] += self.sz[i]
+        else:
+            self.id[j] = i
+            self.sz[i] += self.sz[j]
