e615f59e80e5a08d34bbd476f63637cb50ea0726
Kutuzov Vlad <alphagrimsaw@gmail.com>
2020-02-13 23:48:22 +0200

    First commit

---
+++ QuickFindUF.py
@@ -0,0 +1,37 @@
+# Quick Find algorithm
+
+
+class QuickFindUF:
+    '''Python's (not the best) implementation of union-find algorithm.
+    '''
+
+    def __init__(self, num=0):
+        self.num = num
+        self.id = [i for i in range(num)]
+
+    def setVal(self, num):
+        self.num = num
+        self.id = [i for i in range(num)]
+
+    def connected(self, p, q):
+        return self.id[p] == self.id[q]
+
+    def union(self, p, q):
+        pid = self.id[p]
+        qid = self.id[q]
+        for i in range(self.num):
+            if self.id[i] == pid:
+                self.id[i] = qid
+
+
+vlad = QuickFindUF(5)
+print(vlad.id)
+
+vlad.union(4,3)
+print(vlad.id)
+
+vlad.union(3,2)
+print(vlad.id)
+
+vlad.union(2,3)
+print(vlad.id)
---
+++ QuickUnionUF.py
@@ -0,0 +1,26 @@
+class QuickUnionUF:
+    '''Python better implementation of union-find algortihm
+    '''
+
+    def __init__(self, num=0):
+        self.num = num
+        self.id = [i for i in range(num)]
+
+    def setNum(self, num):
+        self.num = num
+        self.id = [i for i in range(num)]
+
+    def __root(self, i):
+        while i != self.id[i]:
+            i = self.id[i]
+        return i
+
+    def connected(self, p, q):
+        return self.__root(p) == self.__root(q)
+
+    def union(self, p, q):
+        i = self.__root(p)
+        j = self.__root(q)
+        self.id[i] = j
+
+
